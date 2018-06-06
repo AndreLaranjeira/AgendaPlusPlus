fc2de6661761b37e37e5572f3d41813568a2ad59
André Laranjeira <andrecaldaslaranjeira@gmail.com>
2018-06-06 14:34:58 -0400

    Changed UserTask's Table name and attributes to reflect database.

--- AgendaServer/src/main/kotlin/model/UserTask.kt
+++ AgendaServer/src/main/kotlin/model/UserTask.kt
@@ -3,12 +3,13 @@ package model
 import org.jetbrains.exposed.sql.*
 import org.joda.time.DateTime
 
-object UserTasks : Table(){
+object User_Task : Table() {
     val id_task = integer("id_task").primaryKey().autoIncrement()
     val task_title = varchar("task_title", 50)
     val task_description = text("task_description")
-    val task_limit = datetime("task_limit")
+    val task_limit = datetime("task_limit").nullable()
     val task_done = datetime("task_done").nullable()
+    val fk_user = integer("fk_user").references(User.id_user)
 }
 /*CREATE TABLE IF NOT EXISTS `AgendaPlusPlus`.`User_Task` (
 `id_task` BIGINT NOT NULL AUTO_INCREMENT,
@@ -30,14 +31,16 @@ data class UserTask (
         val id_task : Int,
         val task_title : String,
         val task_description : String,
-        val task_limit : DateTime,
-        val task_done : DateTime?
+        val task_limit : DateTime?,
+        val task_done : DateTime?,
+        val fk_user : Int
 )
 
 data class NewUserTask(
         val id_task : Int?,
         val task_title : String,
         val task_description : String,
-        val task_limit : DateTime,
-        val task_done : DateTime?
+        val task_limit : DateTime?,
+        val task_done : DateTime?,
+        val fk_user : Int
 )
--- AgendaServer/src/main/kotlin/service/UserTaskService.kt
+++ AgendaServer/src/main/kotlin/service/UserTaskService.kt
@@ -8,12 +8,12 @@ import service.DatabaseFactory.dbQuery
 class UserTaskService {
 
     suspend fun getAllUserTasks(): List<UserTask> = dbQuery {
-        UserTasks.selectAll().map { toUserTask(it) }
+        User_Task.selectAll().map { toUserTask(it) }
     }
 
     suspend fun getUserTask(id: Int): UserTask? = dbQuery {
-        UserTasks.select {
-            (UserTasks.id_task eq id)
+        User_Task.select {
+            (User_Task.id_task eq id)
         }.mapNotNull { toUserTask(it) }
                 .singleOrNull()
     }
@@ -24,10 +24,11 @@ class UserTaskService {
             addUserTask(UserTask)
         } else {
             dbQuery {
-                UserTasks.update({ UserTasks.id_task eq id }) {
+                User_Task.update({ User_Task.id_task eq id }) {
                     it[task_title] = UserTask.task_title
                     it[task_description] = UserTask.task_description
                     it[task_limit] = UserTask.task_limit
+                    it[fk_user] = UserTask.fk_user
                 }
             }
             getUserTask(id)!!
@@ -37,25 +38,27 @@ class UserTaskService {
     suspend fun addUserTask(UserTask: NewUserTask): UserTask {
         var key: Int? = 0
         dbQuery {
-            key = UserTasks.insert({
+            key = User_Task.insert({
                 it[task_title] = UserTask.task_title
                 it[task_description] = UserTask.task_description
                 it[task_limit] = UserTask.task_limit
-            }) get UserTasks.id_task
+                it[fk_user] = UserTask.fk_user
+            }) get User_Task.id_task
         }
         return getUserTask(key!!)!!
     }
 
     suspend fun deleteUserTask(id: Int): Boolean = dbQuery {
-        UserTasks.deleteWhere { UserTasks.id_task eq id } > 0
+        User_Task.deleteWhere { User_Task.id_task eq id } > 0
     }
 
     private fun toUserTask(row: ResultRow): UserTask =
             UserTask(
-                    id_task = row[UserTasks.id_task],
-                    task_title = row[UserTasks.task_title],
-                    task_description = row[UserTasks.task_description],
-                    task_limit = row[UserTasks.task_limit],
-                    task_done = row[UserTasks.task_done]
+                    id_task = row[User_Task.id_task],
+                    task_title = row[User_Task.task_title],
+                    task_description = row[User_Task.task_description],
+                    task_limit = row[User_Task.task_limit],
+                    task_done = row[User_Task.task_done],
+                    fk_user = row[User_Task.fk_user]
             )
 }
